fbec55afc2f2068b7cf1a39b66f5269cc1119ec3
Hick <124798752+hick-hpe@users.noreply.github.com>
2025-08-21 20:08:41 -0300

    feat: instalação e configuração do Java FX no VS Code

--- run.sh
+++ run.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# Caminhos
-SRC_DIR="src"
-OUT_DIR="out"
-MAIN_CLASS="Main"
-JAVAFX_PATH="/home/aluno/javafx-sdk-21.0.5/lib"
+# compilar    
+javac --module-path lib --add-modules javafx.controls -d out src/Main.java
 
-# Cria pasta de saída se não existir
-mkdir -p "$OUT_DIR"
-
-# Compilar
-echo "Compilando..."
-javac --module-path "$JAVAFX_PATH" --add-modules javafx.controls -d "$OUT_DIR" "$SRC_DIR/$MAIN_CLASS.java"
-
-# Verifica se compilação teve sucesso
-if [ $? -ne 0 ]; then
-    echo "❌ Erro na compilação."
-    exit 1
-fi
-
-# Executar
-echo "Executando..."
-java --module-path "$JAVAFX_PATH" --add-modules javafx.controls -cp "$OUT_DIR" "$MAIN_CLASS"
-
+# executar
+java --module-path lib --add-modules javafx.controls -cp out Main
+    
